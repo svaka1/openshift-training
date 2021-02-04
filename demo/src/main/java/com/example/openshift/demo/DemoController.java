2182483b6ba2db160448d0405c984a205008399a
svaka1 <44224420+svaka1@users.noreply.github.com>
2021-02-04 16:01:44 +0530

    demo updated

--- demo/src/main/java/com/example/openshift/demo/DemoController.java
+++ demo/src/main/java/com/example/openshift/demo/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoController {
 
-    @RequestMapping("/he")
+    @RequestMapping("/hel")
     public String home() {
         return "Hello World - v3!";
     }
